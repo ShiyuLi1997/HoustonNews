3369f5375d2d720b77103d947fc0a798af739d2a
Caleb Li <40342681+ShiyuLi1997@users.noreply.github.com>
2023-06-13 01:15:23 -0500

    init sign on. worked. need signin page. and refactor signon code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, Typography } from "@mui/material";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 import CustomInputLabel from "./components/CustomInputLabel.jsx";
 // import Login from "./components/Login.jsx";
-const App = () => {
-	const [email, setEmail] = React.useState("");
-	const [password, setPassword] = React.useState("");
-	return (
-		<Grid className="root-container">
-			<CustomInputLabel
-				type="none"
-				label="Email"
-				smallMSG="We do not share your email address"
-				onInputChange={(change) => {
-					setEmail(change);
-				}}
-			/>
-			<CustomInputLabel
-				type="none"
-				label="Password"
-				smallMSG="We got top of the bar security"
-				onInputChange={(change) => {
-					setPassword(change);
-				}}
-			/>
-			<Typography variant="h6">Email Entered: {email}</Typography>
-			<Typography variant="h6">Password Entered: {password}</Typography>
-			{/* <Login /> */}
-		</Grid>
-	);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBXO8uQeF08I6jyAMZaPMNyZz1fS42Srk",
+  authDomain: "meinanproject.firebaseapp.com",
+  projectId: "meinanproject",
+  storageBucket: "meinanproject.appspot.com",
+  messagingSenderId: "505519465081",
+  appId: "1:505519465081:web:42eb7b4dce1471222e2aa8",
+  measurementId: "G-6BZ4LSKZJE",
+};
+
+const App = (params) => {
+  // Initialize Firebase Authentication and get a reference to the service
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  console.log({ auth, app });
+
+  const signon = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log({ user, userCredential });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, errorMessage);
+        // ..
+      });
+  };
+
+  return (
+    <Grid className="root-container">
+      <CustomInputLabel
+        type="none"
+        label="Email"
+        smallMSG="We do not share your email address"
+        onInputChange={(change) => {
+          setEmail(change);
+        }}
+      />
+      <CustomInputLabel
+        type="none"
+        label="Password"
+        smallMSG="We got top of the bar security"
+        onInputChange={(change) => {
+          setPassword(change);
+        }}
+      />
+      <Typography variant="h6">Email Entered: {email}</Typography>
+      <Typography variant="h6">Password Entered: {password}</Typography>
+      <button onClick={signon}>Sign On</button>
+      {/* <Login /> */}
+    </Grid>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -10,24 +10,9 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: "AIzaSyBBXO8uQeF08I6jyAMZaPMNyZz1fS42Srk",
-	authDomain: "meinanproject.firebaseapp.com",
-	projectId: "meinanproject",
-	storageBucket: "meinanproject.appspot.com",
-	messagingSenderId: "505519465081",
-	appId: "1:505519465081:web:42eb7b4dce1471222e2aa8",
-	measurementId: "G-6BZ4LSKZJE",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-console.log({ app, analytics });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
